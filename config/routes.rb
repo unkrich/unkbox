b3525f585d2082bef96778d29680c37b7f11e6ed
Kevin Unkrich <kev.unkrich@gmail.com>
2019-03-24 19:14:25 -0400

    Added first and last name to User model, and added sign up form to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
