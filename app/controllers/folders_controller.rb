cc9b8cc0871b0378d16751e1f14fbcc9b3483089
Kevin Unkrich <kev.unkrich@gmail.com>
2019-04-01 23:07:05 -0400

    Added HTML/CSS for delete button and routes/controller logic for deleting folders and files

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,6 +1,6 @@
 class FoldersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_folder, only: [:update]
+  before_action :set_folder, only: [:update, :destroy, :destroy_file]
 
   def create
   	@folder = current_user.folders.new(folder_params)
@@ -12,6 +12,18 @@ class FoldersController < ApplicationController
 
   def update
     if @folder.update(folder_params)
+      redirect_to '/home'
+    end
+  end
+
+  def destroy
+    if @folder != current_user.home and @folder.files.purge_later and @folder.destroy
+      redirect_to '/home'
+    end
+  end
+
+  def destroy_file
+    if @folder.files.find(params[:folder_id]).purge_later
       redirect_to '/home'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  # Main user-facing routes
   root 'pages#landing'
   get '/home', to: 'pages#home'
+  get '/home/*path', :controller => 'pages', :action => 'home'
 
+  # devise sign up/in
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
 
+  # folder/file routes
   resources :folders
-  get '/home/*path', :controller => 'pages', :action => 'home'
+  get '/folders/:id/files/:folder_id', to: 'folders#destroy_file'
 end
