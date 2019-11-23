ead777a1214a1dddd374f5cfe3ab65cc2029f947
Kevin Unkrich <kev.unkrich@gmail.com>
2019-11-23 10:34:56 -0800

    Unitialized Constant error on including ActiveStorage:SetCurrent. Removed it, working as expected

--- app/controllers/active_storage/base_controller.rb
+++ app/controllers/active_storage/base_controller.rb
@@ -1,6 +1,5 @@
 class ActiveStorage::BaseController < ActionController::Base
   before_action :authenticate_user!
-  include ActiveStorage::SetCurrent
 
   protect_from_forgery with: :exception
 end
